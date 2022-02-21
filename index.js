7503c47908d513c57e85d1bd0766a38534c72900
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-02-21 14:54:38 +0900

    初期コミット

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const process = require('child_process');
+
+try {
+  const username = core.getInput('username');
+  const password = core.getInput('password');
+
+  process.execFile('security', ['list-keychain -d user -s ~/Library/Keychains/login.keychain-db'], (error, stdout, stderr) => {
+    if (error) {
+      console.log('ERROR', error);
+      core.setFailed(error);
+    } else {
+      console.log('STDOUT', stdout);
+      console.log('STDERR', stderr);
+    }
+  })
+} catch (ex) {
+  core.setFailed(ex.message);
+}
+

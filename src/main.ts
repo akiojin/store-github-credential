ad6c45051103889bc253fe0d1b67894a9bbf1a13
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-16 00:57:41 +0900

    Security を削除して @akiojin/keychain を利用するように変更

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import * as os from 'os'
 import { GitCredentialManagerCore as Credential } from './GitCredentialManagerCore'
-import { Security } from './Security'
+import { Keychain } from '@akiojin/keychain'
 import { BooleanStateValue, StringStateValue } from './StateHelper'
 
 const IsMacOS = os.platform() === 'darwin'
 
 const PostProcess = new BooleanStateValue('IS_POST_PROCESS')
-const Keychain = new StringStateValue('KEYCHAIN')
+const KeychainCache = new StringStateValue('KEYCHAIN')
+const KeychainPasswordCache = new StringStateValue('KEYCHAIN_PASSWORD')
 
 async function Run()
 {
@@ -27,24 +28,34 @@ async function Run()
 			keychain = `${process.env.HOME}/Library/Keychains/login.keychain-db`
 		}
 
-		const keychainPassword: string = core.getInput('keychain-password')
-		if (keychainPassword !== '') {
-			core.setSecret(keychainPassword)
-			await Security.UnlockKeychain(keychain, keychainPassword)
+		core.startGroup('Keychain Settings')
+		{
+			const keychainPassword: string = core.getInput('keychain-password')
+			if (keychainPassword !== '') {
+				KeychainPasswordCache.Set(keychainPassword)
+				core.setSecret(keychainPassword)
+				await Keychain.UnlockKeychain(keychain, keychainPassword)
+			}
+
+			await Keychain.SetDefaultKeychain(keychain)
+			await Keychain.SetListKeychain(keychain)
+			await Keychain.SetDefaultKeychain(keychain)
+			await Keychain.SetListKeychain(keychain)
+
+			KeychainCache.Set(keychain)
+
+			await Keychain.ShowDefaultKeychain()
+			await Keychain.ShowListKeychains()
 		}
+		core.endGroup()
 
-		await Security.SetDefaultKeychain(keychain)
-		await Security.SetListKeychains(keychain)
-
-		await Security.SetDefaultKeychain(keychain)
-		await Security.SetListKeychains(keychain)
-
-		await Security.ShowDefaultKeychain()
-		await Security.ShowListKeychains()
-
-		await Credential.Configure()
-		await Credential.Store(githubUsername, githubPassword)
-		await Credential.Get()
+		core.startGroup('git credential-manager-core Settings')
+		{
+			await Credential.Configure()
+			await Credential.Store(githubUsername, githubPassword)
+			await Credential.Get()
+		}
+		core.endGroup()
 	} catch (ex: any) {
 		core.setFailed(ex.message)
 	}
@@ -53,7 +64,8 @@ async function Run()
 async function Cleanup()
 {
 	try {
-		await Security.SetDefaultKeychain(Keychain.Get())
+		await Keychain.SetDefaultKeychain(KeychainCache.Get())
+		await Keychain.UnlockKeychain(KeychainCache.Get(), KeychainPasswordCache.Get())
 		await Credential.Erase()
 	} catch (ex: any) {
 		core.setFailed(ex.message)
